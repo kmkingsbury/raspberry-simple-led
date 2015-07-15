eb61addb3f4a6384a5aae00da1ab790de7668263
Kevin Kingsbury <kkingsbury@gmail.com>
2015-07-15 08:49:48 -0500

    Update light.py
    More leds

--- light.py
+++ light.py
@@ -3,14 +3,32 @@ import time ## Import 'time' library.  Allows us to use 'sleep'
 
 GPIO.setmode(GPIO.BOARD) ## Use BOARD pin numbering
 GPIO.setup(7, GPIO.OUT) ## Setup GPIO pin 7 to OUT
+GPIO.setup(8, GPIO.OUT) ## Setup GPIO pin 8 to OUT
+GPIO.setup(10, GPIO.OUT) ## Setup GPIO pin 10 to OUT
+GPIO.setup(11, GPIO.OUT) ## Setup GPIO pin 11 to OUT
+GPIO.setup(12, GPIO.OUT) ## Setup GPIO pin 12 to OUT
+
 
 ## Define function named Blink()
 def Blink(numTimes, speed):
     for i in range(0,numTimes): ## Run loop numTimes
         print "Iteration " + str(i+1) ##Print current loop
+        
         GPIO.output(7, True) ## Turn on GPIO pin 7
         time.sleep(speed) ## Wait
         GPIO.output(7, False) ## Switch off GPIO pin 7
+        GPIO.output(8, True) ## Turn on GPIO pin 7
+        time.sleep(speed) ## Wait
+        GPIO.output(8, False) ## Switch off GPIO pin 7
+        GPIO.output(10, True) ## Turn on GPIO pin 7
+        time.sleep(speed) ## Wait
+        GPIO.output(10, False) ## Switch off GPIO pin 7
+        GPIO.output(11, True) ## Turn on GPIO pin 7
+        time.sleep(speed) ## Wait
+        GPIO.output(11, False) ## Switch off GPIO pin 7
+        GPIO.output(12, True) ## Turn on GPIO pin 7
+        time.sleep(speed) ## Wait
+        GPIO.output(12, False) ## Switch off GPIO pin 7
         time.sleep(speed) ## Wait
     print "Done" ## When loop is complete, print "Done"
     GPIO.cleanup()
